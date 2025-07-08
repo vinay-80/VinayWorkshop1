f41008ff6fdc13bc2849ac3be928881e8af9a6a6
Vinay-80 <vinay02273@gmail.com>
2025-07-08 15:59:39 +0530

    again updated

--- src/dsaprj/mca/dsa/Student.java
+++ src/dsaprj/mca/dsa/Student.java
@@ -1,9 +1,11 @@
 package dsaprj.mca.dsa;
 
 public class Student {
-
-	public static void main(String[] args) {
-		System.out.println("hello bro");
+	 public static void main(String[] args) {
+		 Product p =new Product("Desktop", 100000, 2000);
+		 System.out.println(p.hashCode());
 	}
+	
+	
 
 }
